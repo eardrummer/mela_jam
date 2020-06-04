f7f69149fb9825a4f9b0be612e6c7e9c003cc8ae
eardrummer <manaswimishra17@gmail.com>
2020-06-04 05:11:00 -0500

    basic visuals and scan line

--- public/sketch.js
+++ public/sketch.js
@@ -1,13 +1,37 @@
 var socket;
 var numClients = 1;
 
+var w_main, h_main;
+
+var scan_x;
+
+var points_main = [];
+var points_other = [];
+
+var clearButton;
+var tempoSlider;
+
 function setup() {
- 	createCanvas(600, 400);
+ 	createCanvas(windowWidth, windowHeight);
   background(51);
 	socket = io.connect();
 
 	socket.on('numClients', clientCounter);
 	socket.on('mouse', newDrawing);
+
+	w_main = windowWidth * 0.6;
+	h_main = windowHeight * 0.3;
+	scan_x = windowWidth/2 - w_main/2;
+
+
+	clearButton = createButton("clear");
+	clearButton.position(windowWidth/2 + w_main * 0.45, windowHeight/2 + h_main*0.6);
+	clearButton.mousePressed(clearPoints);
+
+	tempoSlider = createSlider(0.5, 10, 2);
+	tempoSlider.position(windowWidth/2 - w_main/2, windowHeight/2 + h_main*0.6);
+
+	frameRate(30);
 }
 
 function clientCounter(n){
@@ -16,26 +40,68 @@ function clientCounter(n){
 }
 
 function newDrawing(data){
-  fill(200,0,0);
-	ellipse(data.x, data.y, 10, 10);
+
+	points_other.push(createVector(data.x, data.y));
 }
 
 function mouseDragged(){
 
-	fill(255);
-	ellipse(mouseX, mouseY, 10, 10)
+	stroke(0,0,0);
+	noFill();
+	rect(windowWidth/2 - w_main/2, windowHeight/2 - h_main/2, w_main, h_main);
 
-	var data = {
-		x: mouseX,
-		y: mouseY
+	if(mouseX > (windowWidth/2 - w_main/2) + 10 && mouseX < (windowWidth/2 + w_main/2) - 10  && mouseY > (windowHeight/2 - h_main/2) + 10 && mouseY < (windowHeight/2 + h_main/2) - 10){
+
+		points_main.push(createVector(mouseX, mouseY));
+
+		var data = {
+			x: mouseX,
+			y: mouseY
+		}
+		socket.emit('mouse', data);
 	}
-	socket.emit('mouse', data);
 }
 
 function draw() {
+
+	background(51);
 
 	noStroke();
 	fill(255)
 	text("Number of jammers : " + numClients, 20, 20 )
 
+	text("Tempo:", windowWidth/2 - w_main/2, windowHeight/2 + h_main*0.6);
+
+	stroke(0,0,0);
+	noFill();
+	rect(windowWidth/2 - w_main/2, windowHeight/2 - h_main/2, w_main, h_main);
+
+	stroke(0,0,100);
+	line(scan_x, windowHeight/2 - h_main/2, scan_x, windowHeight/2 + h_main/2);
+
+	updateScanX();
+
+	// Draw the points
+	for(let i = 0; i < points_main.length; i++){
+		fill(255);
+		ellipse(points_main[i].x, points_main[i].y, 10, 10)
+	}
+
+	for(let i = 0; i < points_other.length; i++){
+		fill(200,0,0);
+		ellipse(points_other[i].x, points_other[i].y, 10, 10);
+	}
+
+}
+
+function updateScanX(){
+	scan_x += tempoSlider.value();
+
+	if(scan_x > windowWidth/2 + w_main/2){
+		scan_x = windowWidth/2 - w_main/2;
+	}
+}
+
+function clearPoints(){
+	points_main = [];
 }
